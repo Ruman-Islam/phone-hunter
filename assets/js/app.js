196e4f7ff8a2523042df245a82d2f790dfa63586
Ruman-Islam <rumanislam48@gmail.com>
2022-03-01 00:06:37 +0600

    loading spinner added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,21 @@ const errorHandler = (error) => {
     phonesContainer.innerHTML = `<h1 class="text-center">${error}</h1>`;
 };
 
-const clearPreviousData = () => {
-    phonesContainer.textContent = '';
+const clearPreviousData = (param) => {
+    phonesContainer.textContent = param;
+};
+
+const spinnerToggler = (style) => {
+    document.querySelector('.spinner').style.display = style;
 };
 
 // Function of Load all Phones
 const loadProducts = async () => {
+    clearPreviousData('');
     if (searchBox.value === '') {
         errorHandler('You typed nothing');
     } else {
-        clearPreviousData();
+        spinnerToggler('block');
         try {
             const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox.value}`;
             const response = await fetch(url);
@@ -29,14 +34,16 @@ const loadProducts = async () => {
         } catch (error) {
             errorHandler(error);
         }
+        spinnerToggler('none');
     }
+    searchBox.value = '';
 };
 
 
-// Function of Display Relevant Phones
+// Function of Display Desired Phones
 const displayProducts = phones => {
     phones.forEach(phone => {
-        // Destructuring info
+        // Destructuring here from phone
         const { brand, phone_name, slug, image } = phone;
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col-md-3');
@@ -44,8 +51,13 @@ const displayProducts = phones => {
         <div class="card border-0 mx-auto" style="width: 14rem;">
             <img src="${image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone_name}</h5>
                 <p class="card-text">${brand}</p>
+                <div class="d-flex justify-content-between">
+                    <h6 class="card-title">${phone_name}</h6>
+                    <p class="card-text text-primary" style="cursor: pointer">
+                        more
+                    </p>
+                </div>
             </div>
         </div>
         `;
