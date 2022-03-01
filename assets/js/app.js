ba3d2cc458b2f0112e8ec7927fcd5113f31b0492
Ruman-Islam <rumanislam48@gmail.com>
2022-03-02 04:03:24 +0600

    all loading data merging problem fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 // Some useful variables //
 const searchBox = document.querySelector('.search__box');
-const phonesContainer = document.querySelector('.phones__container');
-const phoneDetailContainer = document.querySelector('.phone__detail');
+const productsContainer = document.querySelector('.products__container');
+const productDetailContainer = document.querySelector('.product__detail');
 const loadButton = document.querySelector('.load__btn');
-let newPhones = [];
+let totalProducts = [];
 //............................//
 //............................//
 
 //....Some utilities functions.....//
 //..Function of set error message as inner html..//
 const errorHandler = (error) => {
-    phonesContainer.innerHTML = `<h2 class="text-center mt-5">${error}</h2>`;
+    productsContainer.innerHTML = `<h2 class="text-center mt-5">${error}</h2>`;
+    loadButton.innerHTML = '';
 };
 //............................//
 
@@ -34,8 +35,8 @@ const searchWithEnter = e => {
 
 // Function of Load all Phones //
 const loadProducts = async () => {
-    phonesContainer.textContent = '';
-    phoneDetailContainer.textContent = '';
+    errorHandler('');
+    productDetailContainer.textContent = '';
     if (searchBox.value === '') {
         errorHandler('type something');
     } else {
@@ -46,7 +47,7 @@ const loadProducts = async () => {
             const data = await response.json();
             if (data.status) {
                 displayProducts(data.data, true);
-                newPhones = data.data;
+                totalProducts = data.data;
             } else {
                 throw 'No result found';
             }
@@ -62,59 +63,59 @@ const loadProducts = async () => {
 
 
 // Function of Display Desired Phones //
-const displayProducts = (phones, isTrue) => {
-    phones = phones.slice(0, 20);
+const displayProducts = (products, isTrue) => {
+    products = products.slice(0, 20);
     isTrue ?
-        phones.forEach(phone => {
+        products.forEach(product => {
             // Destructuring data from phone //
-            const { brand, phone_name, slug, image } = phone;
-            const phoneDiv = document.createElement('div');
-            phoneDiv.classList.add('col-md-3');
-            phoneDiv.innerHTML = `
+            const { brand, phone_name, slug, image } = product;
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('col-md-3');
+            productDiv.innerHTML = `
             <div class="card border-0 mx-auto" style="width: 10rem;">
                 <img src="${image}" class="card-img-top" alt="${phone_name}">
                 <div class="card-body">
                     <p class="card-text">${brand}</p>
                     <div class="d-flex justify-content-between">
                         <h6 class="card-title">${phone_name}</h6>
-                        <a href="#header" onclick="loadPhoneDetail('${slug}')" class="card-text text-primary" style="text-decoration: none">
+                        <a href="#header" onclick="loadproductDetail('${slug}')" class="card-text text-primary" style="text-decoration: none">
                             Detail
                         </a>
                     </div>
                 </div>
             </div>
             `;
-            phonesContainer.appendChild(phoneDiv);
-            if (phones.length >= 20) {
-                loadButton.innerHTML = `
+            productsContainer.appendChild(productDiv);
+            loadButton.innerHTML = `
                     <button onclick="displayProducts('false')" type="button" class="btn btn-outline-dark btn-sm">
                         load more
                     </button>
                 `;
-            }
+            totalProducts = [];
+
         })
         :   // load more product button functionalities //
-        phonesContainer.textContent = '';
-    newPhones.forEach(phone => {
+        errorHandler('');
+    totalProducts.forEach(product => {
         // Destructuring data from phone //
-        const { brand, phone_name, slug, image } = phone;
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('col-md-3');
-        phoneDiv.innerHTML = `
+        const { brand, phone_name, slug, image } = product;
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('col-md-3');
+        productDiv.innerHTML = `
                 <div class="card border-0 mx-auto" style="width: 10rem;">
                     <img src="${image}" class="card-img-top" alt="${phone_name}">
                     <div class="card-body">
                         <p class="card-text">${brand}</p>
                         <div class="d-flex justify-content-between">
                             <h6 class="card-title">${phone_name}</h6>
-                            <a href="#header" onclick="loadPhoneDetail('${slug}')" class="card-text text-primary" style="text-decoration: none">
+                            <a href="#header" onclick="loadproductDetail('${slug}')" class="card-text text-primary" style="text-decoration: none">
                                 Detail
                             </a>
                         </div>
                     </div>
                 </div>
                 `;
-        phonesContainer.appendChild(phoneDiv);
+        productsContainer.appendChild(productDiv);
         loadButton.innerHTML = '';
     });
 };
@@ -123,14 +124,14 @@ const displayProducts = (phones, isTrue) => {
 
 
 // Load Details function //
-const loadPhoneDetail = async slug => {
-    phoneDetailContainer.textContent = '';
+const loadproductDetail = async slug => {
+    productDetailContainer.textContent = '';
     try {
         const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
         const response = await fetch(url);
         const data = await response.json();
         if (data.status) {
-            displayPhoneDetail(data.data);
+            displayProductDetail(data.data);
         } else {
             throw 'Nothing is found';
         }
@@ -143,13 +144,13 @@ const loadPhoneDetail = async slug => {
 
 
 // Function of display phone details //
-const displayPhoneDetail = phone => {
+const displayProductDetail = product => {
     // Destructuring data from phone //
     const { image, name, releaseDate,
         mainFeatures: { chipSet, displaySize, memory, sensors, storage } = {},
-        others: { Bluetooth, GPS, NFC, Radio, USB, WLAN } = {} } = phone;
-    const phoneDetail = document.createElement('div');
-    phoneDetail.innerHTML = `
+        others: { Bluetooth, GPS, NFC, Radio, USB, WLAN } = {} } = product;
+    const productDetail = document.createElement('div');
+    productDetail.innerHTML = `
     <div class="card mb-5 p-2 mx-auto" style="max-width: 1000px;">
         <div class="row text-center">
             <div class="col-12">
@@ -158,36 +159,28 @@ const displayPhoneDetail = phone => {
                 <p class="card-text m-2">${releaseDate ? releaseDate : 'no release date'}</p>
             </div>
         </div>
-        <div class="row g-0">
-            <div class="col-md-6">
-                <div class="card-body">
-                    <ul>
-                        <h6 class="card-text"><strong>Specification</strong></h6>
-                        <li>Chipset: <small>${chipSet}</small></li>
-                        <li>Display: <small>${displaySize}</small></li>
-                        <li>RAM: <small>${memory}</small></li>
-                        <li>Storage: <small>${storage}</small></li>
-                        <li>Sensors: <small>${sensors ? sensors.join(', ') : 'Not available'}</small></li>
-                    </ul>
-                </div>
+            <div class="card-body text-center">
+                <ul>
+                    <h6 class="card-text"><strong>Specification</strong></h6>
+                    <li>Chipset: <small>${chipSet}</small></li>
+                    <li>Display: <small>${displaySize}</small></li>
+                    <li>RAM: <small>${memory}</small></li>
+                    <li>Storage: <small>${storage}</small></li>
+                    <li>Sensors: <small>${sensors ? sensors.join(', ') : 'Not available'}</small></li>
+                </ul>
+                <ul>
+                    <h6 class="card-text"><strong>Others</strong></h6>
+                    <li>Bluetooth: <small>${Bluetooth ? Bluetooth : 'Not available'}</small></li>
+                    <li>GPS: <small>${GPS ? GPS : 'Not available'}</small></li>
+                    <li>NFC: <small>${NFC ? NFC : 'Not available'}</small></li>
+                    <li>FM: <small>${Radio ? Radio : 'Not available'}</small></li>
+                    <li>USB: <small>${USB ? USB : 'Not available'}</small></li>
+                    <li>WLAN: <small>${WLAN ? WLAN : 'Not available'}</small></li>
+                </ul>
             </div>
-            <div class="col-md-6">
-                <div class="card-body">
-                    <ul>
-                        <h6 class="card-text"><strong>Others</strong></h6>
-                        <li>Bluetooth: <small>${Bluetooth ? Bluetooth : 'Not available'}</small></li>
-                        <li>GPS: <small>${GPS ? GPS : 'Not available'}</small></li>
-                        <li>NFC: <small>${NFC ? NFC : 'Not available'}</small></li>
-                        <li>FM: <small>${Radio ? Radio : 'Not available'}</small></li>
-                        <li>USB: <small>${USB ? USB : 'Not available'}</small></li>
-                        <li>WLAN: <small>${WLAN ? WLAN : 'Not available'}</small></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
     </div>
     `;
-    phoneDetailContainer.appendChild(phoneDetail);
+    productDetailContainer.appendChild(productDetail);
 };
 //.......................................................//
 //.......................................................//
